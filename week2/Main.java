76625b0a6f9a711881bcaf417a8cee074e75d3f2
Anwaripasha Akbar <pashaakbar52@gmail.com>
2022-10-15 07:44:39 +0700

    File mentoring

---
+++ week2/Main.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] arsg) {
+
+        // TIPE DATA
+
+        // int a = 1;
+        // float b = 1.4f;
+        // String jurusan = "Teknik Informatika";
+        // char alfabet = 'a';
+        // boolean benar = true;
+
+        // System.out.println(a + " " + ((Object) a).getClass().getSimpleName());
+        // System.out.println(b + " " + ((Object) b).getClass().getSimpleName());
+        // System.out.println(jurusan + " " + ((Object)
+        // jurusan).getClass().getSimpleName());
+        // System.out.println(alfabet + " " + ((Object)
+        // alfabet).getClass().getSimpleName());
+        // System.out.println(benar + " " + ((Object)
+        // benar).getClass().getSimpleName());
+
+        // Operator Aritmatika
+
+        // int angka1, angka2, penjumlahan, pengurangan, perkalian, pembagian, sisabagi;
+        // angka1 = 6;
+        // angka2 = 3;
+        // penjumlahan = angka1 + angka2; // 9
+        // pengurangan = angka1 - angka2; // 3
+        // perkalian = angka1 * angka2; // 18
+        // pembagian = angka1 / angka2; // 2
+        // sisabagi = angka1 % angka2; // 0
+
+        // System.out.println("penjumalahan : " + penjumlahan);
+        // System.out.println("pengurangan : " + pengurangan);
+        // System.out.println("perkalian : " + perkalian);
+        // System.out.println("pembagian : " + pembagian);
+        // System.out.println("Sisa Bagi(Modulus) : " + sisabagi);
+
+        // Operator Unary
+
+        // int a, b;
+        // a = +5;
+        // b = -5;
+        // System.out.println("ini a: " + a);
+        // System.out.println("ini b: " + b);
+        // a++;
+        // b--;
+
+        // System.out.println("ini a setelah increment " + a);
+        // System.out.println("ini b seteleah decrement " + b);
+
+        // Penggabungan Dua String
+
+        // String kalimat, namaKampus, gabung;
+        // kalimat = "Saya berkuliah di ";
+        // namaKampus = "Universitas Sriwijaya";
+        // gabung = kalimat + namaKampus;
+        // System.out.println(gabung);
+
+        // Penggabungan String dengan Numerik
+        // String kalimat, namaKampus, kalimat2, gabung;
+        // int semester;
+        // kalimat = "Saya berkuliah di ";
+        // namaKampus = "Universitas Sriwijaya ";
+        // kalimat2 = "dan memasuki semester ";
+        // semester = 2;
+        // gabung = kalimat + namaKampus + kalimat2 + semester;
+        // System.out.println(gabung);
+
+        // Konversi
+        // Double ke String
+        // double num = 1.5;
+        // String strBaru = Double.toString(num);
+        // System.out.println(strBaru);
+
+        // String ke Double
+        // String angka = "20";
+        // double dblBaru = Double.parseDouble(strBaru);
+        // System.out.println(dblBaru);
+
+        // String to Integer
+        // String num = "34";
+        // int intBaru = Integer.parseInt(num);
+        // System.out.println(num);
+
+        // Integer to Double
+        // int angka = 52;
+        // double doubleBaru = (double) angka;
+        // System.out.println(doubleBaru);
+
+        // Contoh Soal
+
+    }
+}
